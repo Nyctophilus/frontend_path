d3fe9ca366b1d72647daa50f35b26e46f771d2ca
Medo <mohammed.yuossry@gmail.com>
2022-01-18 17:41:09 +0200

    Day4... starting Week-6 off with Arrays💪🥶

---
+++ Javascript-BOOTCAMP/week-6.js
@@ -0,0 +1,107 @@
+/*
+  Arrays
+  - Create Arrays [Two Methods] new Array() + []
+  - Access Arrays Elements
+  - Nested Arrays
+  - Change Arrays Elements
+  - Check For Array Array.isArray(arr);
+*/
+
+let myFriends = [
+  "Ahmed",
+  "Mohamed",
+  "Sayed",
+  ["Marwan", "Ali"],
+];
+
+console.log(`Hello ${myFriends[0]}`);
+console.log(`Hello ${myFriends[2]}`);
+console.log(`${myFriends[1][2]}`);
+console.log(`Hello ${myFriends[3][1]}`);
+console.log(`${myFriends[3][1][2]}`);
+
+console.log(myFriends);
+myFriends[1] = "Gamal";
+console.log(myFriends);
+myFriends[3] = ["Sameh", "Ameer"];
+console.log(myFriends);
+
+console.log(Array.isArray(myFriends));
+
+/*
+  Array Methods
+  - Length
+*/
+
+// Index Start From 0 [ 0, 1, 2, 3, 4 ]
+
+let myFriends = ["Ahmed", "Mohamed", "Sayed", "Alaa"];
+
+myFriends.length = 2;
+
+console.log(myFriends);
+
+/*
+  Arrays Methods [Adding And Removing]
+  - unshift("", "") Add Element To The First
+  - push("", "") Add Element To The End
+  - shift() Remove First Element From Array
+  - pop() Remove Last Element From Array
+*/
+
+let myFriends = ["Ahmed", "Mohamed", "Sayed", "Alaa"];
+
+console.log(myFriends);
+
+myFriends.unshift("Osama", "Nabil");
+
+console.log(myFriends);
+
+myFriends.push("Samah", "Eman");
+
+console.log(myFriends);
+
+let first = myFriends.shift();
+
+console.log(myFriends);
+
+console.log(`First Name Is ${first}`);
+
+let last = myFriends.pop();
+
+console.log(myFriends);
+
+console.log(`Last Name Is ${last}`);
+
+/*
+  Arrays Methods [Search]
+  - indexOf(Search Element, From Index [Opt])
+  - lastIndexOf(Search Element, From Index [Opt])
+  - includes(valueToFind, fromIndex [Opt]) [ES7]
+*/
+
+let myFriends = [
+  "Ahmed",
+  "Mohamed",
+  "Sayed",
+  "Alaa",
+  "Ahmed",
+];
+
+console.log(myFriends);
+
+console.log(myFriends.indexOf("Ahmed"));
+console.log(myFriends.indexOf("Ahmed", 2));
+
+console.log(myFriends.lastIndexOf("Ahmed"));
+console.log(myFriends.lastIndexOf("Ahmed", -2));
+
+console.log(myFriends.includes("Ahmed"));
+console.log(myFriends.includes("Ahmed", 2));
+
+if (myFriends.lastIndexOf("Osama") === -1) {
+  console.log("Not Found");
+}
+
+console.log(myFriends.indexOf("Osama"));
+console.log(myFriends.lastIndexOf("Osama"));
