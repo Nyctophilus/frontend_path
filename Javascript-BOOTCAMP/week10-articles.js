3b2052ef1632c7a783301e240341a99e6b848ddf
Medo <mohammed.yuossry@gmail.com>
2022-01-22 23:04:23 +0200

    Day8... HOF reading articles🏴‍☠️🏴‍☠️🏴‍☠️

---
+++ Javascript-BOOTCAMP/week10-articles.js
@@ -0,0 +1,42 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
+Array.prototype.flat();
+
+const arr1 = [0, 1, 2, [3, 4]];
+
+console.log(arr1.flat());
+// expected output: [0, 1, 2, 3, 4]
+
+const arr2 = [0, 1, 2, [[[3, 4]]]];
+
+console.log(arr2.flat(2));
+// expected output: [0, 1, 2, [3, 4]]
+
+// Syntax
+flat();
+flat(depth);
+
+const arr1 = [1, 2, [3, 4]];
+arr1.flat();
+// [1, 2, 3, 4]
+
+const arr2 = [1, 2, [3, 4, [5, 6]]];
+arr2.flat();
+// [1, 2, 3, 4, [5, 6]]
+
+const arr3 = [1, 2, [3, 4, [5, 6]]];
+arr3.flat(2);
+// [1, 2, 3, 4, 5, 6]
+
+const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+arr4.flat(Infinity);
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// Flattening and array holes
+// The flat method removes empty slots in arrays:
+
+const arr5 = [1, 2, , 4, 5];
+arr5.flat();
+// [1, 2, 4, 5]
+
+// Array.prototype.forEach()
+// The forEach() method executes a provided function once for each array element.
