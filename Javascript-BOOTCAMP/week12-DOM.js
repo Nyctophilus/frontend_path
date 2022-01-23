0c62308c7ee12b91d1429faa3718860de6197787
Medo <mohammed.yuossry@gmail.com>
2022-01-23 16:15:35 +0200

    Day9... 🧰DOM 🔗🔗

---
+++ Javascript-BOOTCAMP/week12-DOM.js
@@ -0,0 +1,134 @@
+/*
+  DOM
+  - What Is DOM
+  - DOM Selectors
+  --- Find Element By ID
+  --- Find Element By Tag Name
+  --- Find Element By Class Name
+  --- Find Element By CSS Selectors
+  --- Find Element By Collection
+  ------ title
+  ------ body
+  ------ images
+  ------ forms
+  ------ links
+*/
+
+let myIdElement = document.getElementById("my-div");
+let myTagElements = document.getElementsByTagName("p");
+let myClassElement =
+  document.getElementsByClassName("my-span");
+let myQueryElement = document.querySelector(".my-span");
+let myQueryAllElement =
+  document.querySelectorAll(".my-span");
+
+console.log(myIdElement);
+console.log(myTagElements[1]);
+console.log(myClassElement[1]);
+console.log(myQueryElement);
+console.log(myQueryAllElement[1]);
+
+console.log(document.title);
+console.log(document.body);
+console.log(document.images);
+console.log(document.forms[0].one.value);
+console.log(document.links[1].href);
+
+/*
+  DOM [Get / Set Elements Content And Attributes]
+  - innerHTML
+  - textContent
+  - Change Attributes Directly
+  - Change Attributes With Methods
+  --- getAttribute
+  --- setAttribute
+
+  Search
+  - innerText
+*/
+
+let myElement = document.querySelector(".js");
+
+console.log(myElement.innerHTML);
+console.log(myElement.textContent);
+
+myElement.innerHTML = "Text From <span>Main.js</span> File";
+myElement.textContent =
+  "Text From <span>Main.js</span> File";
+
+document.images[0].src = "https://google.com";
+document.images[0].alt = "Alternate";
+document.images[0].title = "Picture";
+document.images[0].id = "pic";
+document.images[0].className = "img";
+
+let myLink = document.querySelector(".link");
+
+console.log(myLink.getAttribute("class"));
+console.log(myLink.getAttribute("href"));
+
+myLink.setAttribute("href", "https://twitter.com");
+myLink.setAttribute("title", "Twitter");
+
+/*
+  DOM [Check Attributes]
+  - Element.attributes
+  - Element.hasAttribute
+  - Element.hasAttributes
+  - Element.removeAttribute
+*/
+
+console.log(
+  document.getElementsByTagName("p")[0].attributes
+);
+
+let myP = document.getElementsByTagName("p")[0];
+
+if (myP.hasAttribute("data-src")) {
+  if (myP.getAttribute("data-src") === "") {
+    myP.removeAttribute("data-src");
+  } else {
+    myP.setAttribute("data-src", "New Value");
+  }
+} else {
+  console.log(`Not Found`);
+}
+
+if (myP.hasAttributes()) {
+  console.log(`Has Attributes`);
+}
+
+if (
+  document.getElementsByTagName("div")[0].hasAttributes()
+) {
+  console.log(`Has Attributes`);
+} else {
+  console.log(`Div Has No Attributes`);
+}
+
+/*
+  DOM [Create Elements]
+  - createElement
+  - createComment
+  - createTextNode
+  - createAttribute
+  - appendChild
+*/
+
+let myElement = document.createElement("div");
+let myAttr = document.createAttribute("data-custom");
+let myText = document.createTextNode("Product One");
+let myComment = document.createComment("This Is Div");
+
+myElement.className = "product";
+myElement.setAttributeNode(myAttr);
+myElement.setAttribute("data-test", "Testing");
+
+// Append Comment To Element
+myElement.appendChild(myComment);
+
+// Append Text To Element
+myElement.appendChild(myText);
+
+// Append Element To Body
+document.body.appendChild(myElement);
