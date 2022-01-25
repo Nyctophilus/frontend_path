ee6c48409544c3ed68dca2e1b8a699c035bba596
Medo <mohammed.yuossry@gmail.com>
2022-01-25 04:31:57 +0200

    Day11... DOM ♦️Section2♦️ 🃏Study Session🃏

---
+++ Javascript-BOOTCAMP/week13-DOM part2.js
@@ -0,0 +1,77 @@
+/*
+  DOM [Events Simulation]
+  - click
+  - focus
+  - blur
+*/
+
+let one = document.querySelector(".one");
+let two = document.querySelector(".two");
+
+window.onload = function () {
+  two.focus();
+};
+
+one.onblur = function () {
+  document.links[0].click();
+};
+
+/*
+  DOM [Class List]
+  - classList
+  --- length
+  --- contains
+  --- item(index)
+  --- add
+  --- remove
+  --- toggle
+*/
+
+let element = document.getElementById("my-div");
+
+console.log(element.classList);
+console.log(typeof element.classList);
+console.log(element.classList.contains("osama"));
+console.log(element.classList.contains("show"));
+console.log(element.classList.item("3"));
+
+element.onclick = function () {
+  element.classList.toggle("show");
+};
+
+/*
+  DOM [Class List]
+  - classList
+  --- length
+  --- contains
+  --- item(index)
+  --- add
+  --- remove
+  --- toggle
+*/
+
+let element = document.getElementById("my-div");
+
+console.log(element.classList);
+console.log(typeof element.classList);
+console.log(element.classList.contains("osama"));
+console.log(element.classList.contains("show"));
+console.log(element.classList.item("3"));
+
+element.onclick = function () {
+  element.classList.toggle("show");
+};
+
+/*
+  DOM [Deal With Elements]
+  - before [Element || String]
+  - after [Element || String]
+  - append [Element || String]
+  - prepend [Element || String]
+  - remove
+*/
+
+let element = document.getElementById("my-div");
+let createdP = document.createElement("p");
+
+// element.remove();
