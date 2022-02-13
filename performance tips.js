8a455b415ba596e409f95d3c26bd278d54e70cd8
Medo <mohammed.yuossry@gmail.com>
2022-02-14 00:31:35 +0200

    tips

---
+++ performance tips.js
@@ -0,0 +1,21 @@
+// images
+// image compression 'Optimization '...> using google to find
+
+// attribute loading="lazy" on iamges.. to loading images shown on the screen only
+
+// MediaQuery on images to show em in different sizes on different screen sizes
+
+// ----------------------------
+// files
+// min the number of files as possible ..> use webpack,rollup or Gulp to do so
+
+// lighthouse report
+
+// ----------------------------
+// Convert the Website to ***PWA***
+// - add manifest.json
+// https://www.altexsoft.com/blog/engineering/12-techniques-of-website-speed-optimization-performance-testing-and-improvement-practices/
+// https://moz.com/learn/seo/page-speed
+
+// unnecessary renders
+// ex: react represnted:virtual DOM >> split the dom to components .. able to update the component u want to change only without touching the other components
