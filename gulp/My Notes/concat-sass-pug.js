99dc3c6730e88045fee9fe6a31c40b5eae03a656
Medo <mohammed.yuossry@gmail.com>
2022-03-18 17:44:16 +0200

    Start working with Gulpjs

---
+++ gulp/My Notes/concat-sass-pug.js
@@ -0,0 +1,32 @@
+// new es modular
+import { task, src, dest } from "gulp";
+import concat from "gulp-concat";
+
+// html task
+export const html = () => {
+  return src("build/*.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(dest("dist"));
+};
+
+// css task
+export const css = () => {
+  return (
+    src(["build/css/sass/*.scss"])
+      //  complie with sass
+      .pipe(sass({ outputStyle: "compressed" }))
+      // add prefixers
+      .pipe(autoprefixer("last 2 versions"))
+      //  cancat all files in on file
+      .pipe(concat("test.css"))
+      //   move compiled files to destination
+      .pipe(dest("dist/css"))
+  );
+};
+
+// js task
+export const jsTask = () => {
+  return src("build/js/*.js")
+    .pipe(concat("all.js"))
+    .pipe(dest("dist/js"));
+};
