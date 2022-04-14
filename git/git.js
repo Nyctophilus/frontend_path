0254a5ccdd33b7bdaafb11b26c14028cde8192cb
Medo <mohammed.yuossry@gmail.com>
2022-04-14 05:58:01 +0200

    git branching

--- git/git.js
+++ git/git.js
@@ -1,6 +1,28 @@
 const Git = {
-  pull: "Git pull origin main",
+  pull: "git pull origin main",
   genKey:
     'ssh-keygen -t rsa -b 4096 -C "example@gmail.com"',
   testKey: "ssh -T git@github.com", // to test authentication
+};
+
+// to edit config file
+// git config --edit
+// -HL explore git alias list
+
+// to set alias
+// git config --global alias.'aliasName' checkout
+
+/*
+	git branches
+*/
+
+const branches = {
+  show: "git branch",
+  create: "git branch branchName",
+  CDtoBranch: "git checkout branchName",
+  delete: "git branch -d branchName", // save delete
+  forceDelete: "git branch -D branchName", // force delete even with uncommited changes
+  createAndCheckout: "git checkout -b branchName",
+  rename: "git branch -m branchName",
+  merge: "git merge branchName", // to merge a branch into current branch
 };
