35250cc83818996df31ba2e3adedd70eef9859e3
Medo <mohammed.yuossry@gmail.com>
2022-07-18 20:23:24 +0200

    project: cocktail
    - source files

---
+++ react/learning-process/src/YT-tut-projects/Cocktails/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+import App from "./App";
+import { AppProvider } from "./context";
+
+function index() {
+  return (
+    <AppProvider>
+      <App />
+    </AppProvider>
+  );
+}
+
+export default index;
